19930fadb119b8d853d1490fefae6ac50c956fc0
ma-xiaodong <xdma@163.com>
2020-03-18 21:18:35 +0800

    program in chapter 5

---
+++ chap05/nms.py
@@ -0,0 +1,44 @@
+import numpy as np
+
+def nms(dets, thresh):
+    x1 = dets[:, 0]
+    y1 = dets[:, 1]
+    x2 = dets[:, 2]
+    y2 = dets[:, 3]
+    
+    scores = dets[:, 4]
+    areas = (x2 - x1 + 1) * (y2 - y1 + 1)
+    order = scores.argsort()[::-1]
+    keep = []
+
+    print(order)
+    while order.size > 0:
+        i = order[0]
+        keep.append(i)
+
+        xx1 = np.maximum(x1[i], x1[order[1:]])
+        yy1 = np.maximum(y1[i], y1[order[1:]])
+        xx2 = np.minimum(x2[i], x2[order[1:]])
+        yy2 = np.minimum(y2[i], y2[order[1:]])
+
+        w = np.maximum(0.0, xx2 - xx1 + 1)
+        h = np.maximum(0.0, yy2 - yy1 + 1)
+
+        inter = w * h
+        ovr = inter / (areas[i] + areas[order[1:]] - inter)
+
+        inds = np.where(ovr <= thresh)[0]
+        order = order[inds + 1]
+        print(ovr, inds)
+
+    return keep
+
+if __name__ == "__main__":
+    dets = np.array([[204, 102, 358, 250, 0.5],
+                     [257, 118, 380, 250, 0.7],
+                     [280, 135, 400, 250, 0.6],
+                     [255, 118, 360, 235, 0.7]])
+    thresh = 0.3
+    bounding_box = nms(dets, thresh)
+    print(bounding_box)
+
